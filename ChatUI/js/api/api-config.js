2a757d5cb5d323a54cf96fba38011cc117851026
Mahdi Luqman Abdulrahman <mahdy.00523775@gmail.com>
2026-08-29 20:55:13 +0300

    Bump ChatUI version to 2.3

--- ChatUI/js/api/api-config.js
+++ ChatUI/js/api/api-config.js
@@ -22,7 +22,7 @@ import {
 import { normalizeMultilineApiKeyText } from './text-api-key-input.js';
 
 export const DEFAULT_GOOGLE_BASE_URL = 'https://generativelanguage.googleapis.com';
-export const CHATUI_VERSION = '2.2';
+export const CHATUI_VERSION = '2.3';
 
 let validationTimer = null;
 let validationController = null;
